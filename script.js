348ef7e63bc297c8829a6dc2762de58e8b112ff1
hoangvubeo <tranhoangvx3@gmail.com>
2025-02-21 14:04:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="vubeo" && p=="vubeogay")
+  {
+    window.open("ho.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
